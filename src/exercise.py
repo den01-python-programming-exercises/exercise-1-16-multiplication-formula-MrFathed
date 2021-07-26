57c7ff2856e6551bb4c59297faf844c4a75a702b
Justin Turner <jmturner638@gmail.com>
2021-07-26 03:10:57 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    first = int(input("Give the first number:"))
+    second = int(input("Give the second number:"))
+    product = first * second
+
+    print(str(first) + " * " + str(second) + " = " + str(product))
 
 if __name__ == '__main__':
     main()
